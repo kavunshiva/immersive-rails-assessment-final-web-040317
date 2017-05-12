e52eb2d887c40297a0e25d9298ca10fa00e9350d
kavunshiva <kavunshiva@gmail.com>
2017-05-12 10:48:26 -0400

    fulfill assignment requirements

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -1,7 +1,16 @@
 class AppearancesController < ApplicationController
 
+  def index
+    @appearances = Appearance.all
+  end
+
+  def show
+    @appearance = Appearance.find_by(id: params[:id])
+    redirect_to :index unless @appearance
+  end
+
   def new
-    @appearance = Appearance.new
+    @appearance = Appearance.new(id: session[:user_id])
   end
 
   def create
@@ -13,10 +22,25 @@ class AppearancesController < ApplicationController
     end
   end
 
+  def edit
+    @appearance = Appearance.find_by(id: params[:id])
+  end
+
+  def update
+    @appearance = Appearance.find_by(id: params[:id])
+    if @appearance.update(appearance_params)
+      redirect_to appearance_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def appearance_params
-    params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    params.require(:appearance).permit(
+      :guest_id, :episode_id, :user_id, :rating
+    )
   end
 
 end
